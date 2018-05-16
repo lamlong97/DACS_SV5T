935e328032fc896f7c25ddf86c97917af46a224a
Lâm Thành Long <lamthanhlong70@gmail.com>
2018-05-16 10:00:49 +0700

    MyCommnit
    Update: Xử lý phần hiển thị thông tin sinh viên

--- dacs_sv5t/Controllers/SinhVienController.cs
+++ dacs_sv5t/Controllers/SinhVienController.cs
@@ -2,7 +2,9 @@
 using DACS_SV5T.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +26,7 @@ namespace DACS_SV5T.Controllers
             var user = _db.SINHVIENs.SingleOrDefault(x => x.MSSV == mssv && x.PASSWORD == passwordMD5);
             if (user != null)
             {
+                Session["ID_SV"] = user.ID_SV;
                 Session["TEN_SV"] = user.TEN_SV;
                 Session["MSSV"] = user.MSSV;
                 Session["LOP"] = user.LOP;
@@ -51,5 +54,49 @@ namespace DACS_SV5T.Controllers
             Session["PASSWORD"] = null;
             return RedirectToAction("Index", "Default");
         }
+
+        public ActionResult ThongTinSV()
+        {
+            var id = Session["ID_SV"];
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SINHVIEN sINHVIEN = _db.SINHVIENs.Find(id);
+            if (sINHVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_KV = new SelectList(_db.KHOAVIENs, "ID_KV", "TEN_KV", sINHVIEN.ID_KV);
+            ViewBag.ID_CAP = new SelectList(_db.CAPs, "ID_CAP", "TEN_CAP", sINHVIEN.ID_CAP);
+            return View(sINHVIEN);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThongTinSV([Bind(Include = "ID_SV,TEN_SV,LOP,DIACHI,EMAIL,SDT,ID_KV,PASSWORD,MSSV,ID_CAP")] SINHVIEN sINHVIEN)
+        {
+            var password = Session["PASSWORD"];
+            if (ModelState.IsValid)
+            {
+                sINHVIEN.PASSWORD = (string)password;
+                _db.Entry(sINHVIEN).State = EntityState.Modified;
+                _db.SaveChanges();
+                ViewBag.success = "Cập nhật thông tin thành công";
+            }
+            ViewBag.ID_KV = new SelectList(_db.KHOAVIENs, "ID_KV", "TEN_KV", sINHVIEN.ID_KV);
+            ViewBag.ID_CAP = new SelectList(_db.CAPs, "ID_CAP", "TEN_CAP", sINHVIEN.ID_CAP);
+            return View(sINHVIEN);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
